b7f5ea08a004cdc5d3ff58f0c006b34b2a715fc1
Audun Sølvberg <audun.solvberg@computas.com>
2018-08-17 15:30:32 +0200

    Added unit tests for extensions and config.

---
+++ test/Hafslund.Akka.Persistence.Bigtable.Tests/BigtablePersistenceTest.cs
@@ -0,0 +1,36 @@
+﻿using Akka.TestKit.Xunit2;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Hafslund.Akka.Persistence.Bigtable.Tests
+{
+    public class BigtablePersistenceTest : TestKit
+    {
+        [Fact]
+        public void BigtablePersistence_NoConfig_ShouldUseDefault()
+        {
+            var bigtablePersistence = BigtablePersistence.Get(Sys);
+
+            Assert.Empty(bigtablePersistence.BigtableJournalSettings.TableName);
+            Assert.Empty(bigtablePersistence.BigtableSnapshotSettings.TableName);
+        }
+
+        [Fact]
+        public void BigtablePersistence_Get_ShouldAddExtension()
+        {
+            var bigtablePersistence = BigtablePersistence.Get(Sys);
+            Assert.NotNull(bigtablePersistence);
+            Assert.True(Sys.HasExtension<BigtablePersistence>());
+        }
+
+        [Fact]
+        public void BigtablePersistence_DefaultConfig_ShouldLoad()
+        {
+            var defaultConfig = BigtablePersistence.DefaultConfig;
+            Assert.True(defaultConfig.HasPath("akka.persistence.journal.bigtable"));
+            Assert.True(defaultConfig.HasPath("akka.persistence.snapshot-store.bigtable"));
+        }
+    }
+}
---
+++ test/Hafslund.Akka.Persistence.Bigtable.Tests/BigtableSettingsTest.cs
@@ -0,0 +1,58 @@
+﻿using Akka.Configuration;
+using Akka.TestKit.Xunit2;
+using Xunit;
+
+namespace Hafslund.Akka.Persistence.Bigtable.Tests
+{
+    public class BigtableSettingsTest : TestKit
+    {
+        [Fact]
+        public void ShouldParseDefaultSnapshotConfig()
+        {
+            var snapshotSettings =
+                BigtableSettings.Create(
+                    ConfigurationFactory.ParseString(@"akka.persistence.snapshot-store.bigtable {
+                        table-name = foo
+                    }").WithFallback(BigtablePersistence.DefaultConfig)
+                        .GetConfig("akka.persistence.snapshot-store.bigtable"));
+
+            Assert.Equal("foo", snapshotSettings.TableName);
+        }
+
+        [Fact]
+        public void BigtablePersistence_CustomJournalConfig_ShouldParseConfig()
+        {
+            var config = ConfigurationFactory.ParseString(
+                            @"akka.persistence.journal.bigtable {
+                                table-name = bar
+                            }
+                             akka.persistence.journal.bigtable-sharding {
+                                table-name = foo
+                            }").WithFallback(BigtablePersistence.DefaultConfig);
+
+            var journalSettings = BigtableSettings.Create(config.GetConfig("akka.persistence.journal.bigtable"));
+            var shardingSettings = BigtableSettings.Create(config.GetConfig("akka.persistence.journal.bigtable-sharding"));
+
+            Assert.Equal("bar", journalSettings.TableName);
+            Assert.Equal("foo", shardingSettings.TableName);
+        }
+
+        [Fact]
+        public void BigtablePersistence_CustomSnapshotConfig_ShouldParseConfig()
+        {
+            var config = ConfigurationFactory.ParseString(
+                            @"akka.persistence.snapshot-store.bigtable {
+                                table-name = bar
+                            }
+                             akka.persistence.snapshot-store.bigtable-sharding {
+                                table-name = foo
+                            }").WithFallback(BigtablePersistence.DefaultConfig);
+
+            var snapshotSettings = BigtableSettings.Create(config.GetConfig("akka.persistence.snapshot-store.bigtable"));
+            var shardingSettings = BigtableSettings.Create(config.GetConfig("akka.persistence.snapshot-store.bigtable-sharding"));
+
+            Assert.Equal("bar", snapshotSettings.TableName);
+            Assert.Equal("foo", shardingSettings.TableName);
+        }
+    }
+}
