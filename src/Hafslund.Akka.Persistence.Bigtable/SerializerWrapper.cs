1503454a1cc4829061653b2da95cbc89b0bb37a3
ef <ef@computas.com>
2018-09-20 17:18:24 +0200

    MDM-861: Remove unused class

--- src/Hafslund.Akka.Persistence.Bigtable/SerializerWrapper.cs
+++
@@ -1,9 +0,0 @@
-namespace Hafslund.Akka.Persistence.Bigtable
-{
-    public interface ISerializerWrapper
-    {
-         byte[] ToBinary(object obj);
-
-         T FromBinary<T>(byte[] bytes);
-    }
-}
